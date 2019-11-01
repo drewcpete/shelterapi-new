7ee5cefadf2d280fc32d6931ee03388998272686
Student <example@example.com>
2019-11-01 14:30:51 -0700

    troubleshooting versioning errors on values controller

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,66 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-namespace AnimalShelterApi.Controllers
+ 
+namespace ApiVersioningSampleApp.Controllers
 {
-
-    [ApiVersion("1.0")]
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesV1Controller : ControllerBase
-    {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1 from Version 1", "value2 from Version 1" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value from Verson 2";
-        }
-
-        // POST api/values
-        // [HttpPost]
-        // public void Post([FromBody] string value)
-        // {
-        // }
-
-        // PUT api/values/5
-        // [HttpPut("{id}")]
-        // public void Put(int id, [FromBody] string value)
-        // {
-        // }
-
-        // DELETE api/values/5
-        // [HttpDelete("{id}")]
-        // public void Delete(int id)
-        // {
-        // }
-    }
-
-
-    [ApiVersion("2.0")]
-    [Route("api/ValuesV2")]
-    [ApiController]
-    public class ValuesV2Controller : ControllerBase
-    {
-        // GET api/values
-        [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
-        {
-            return new string[] { "value1 from Version 2", "value2 from Version 2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value from Version 2";
-        }
-    }
+[ApiVersion("1.0")]
+[Route("api/Values")]
+public class ValuesV1Controller : Controller
+{
+// GET api/values
+[HttpGet]
+public IEnumerable<string> Get()
+{
+return new string[] { "Value1 from Version 1", "value2 from Version 1" };
+}
+}
+ 
+[ApiVersion("2.0")]
+[Route("api/Values")]
+public class ValuesV2Controller : Controller
+{
+// GET api/values
+[HttpGet]
+public IEnumerable<string> Get()
+{
+return new string[] { "value1 from Version 2", "value2 from Version 2" };
+}
+}
 }
